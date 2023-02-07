54bd69523e8a1b3ce072c3bfbc7d896ccb3a197d
DougCodez <dougdevelops@gmail.com>
2023-02-07 11:36:45 -0500

    Added MIT License & Registered Monitors for Velocity

--- wirez-velocity/src/main/java/io/github/dougcodez/wirezvelocity/WireZPlugin.java
+++ wirez-velocity/src/main/java/io/github/dougcodez/wirezvelocity/WireZPlugin.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 Douglas (dougcodez)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package io.github.dougcodez.wirezvelocity;
 
 import com.google.inject.Inject;
@@ -13,6 +36,7 @@ import io.github.dougcodez.wirez.commands.sub.SubCommandRegistry;
 import io.github.dougcodez.wirez.commands.sub.types.impl.database.DumpTable;
 import io.github.dougcodez.wirez.files.types.lang.LangFile;
 import io.github.dougcodez.wirez.files.types.settings.WireZSettingsFile;
+import io.github.dougcodez.wirez.monitors.MonitorManager;
 import io.github.dougcodez.wirez.monitors.task.SystemsThreadExecutor;
 import io.github.dougcodez.wirez.platform.PlatformInfo;
 import io.github.dougcodez.wirez.platform.PlatformType;
@@ -56,6 +80,7 @@ public class WireZPlugin implements WireZPluginExtension, PlatformInfo {
         this.getLogger().log(Level.INFO, getPlatformInfo());
         settingsFile = new WireZSettingsFile();
         settingsFile.initConfigFile(getDataDirectory().toFile());
+        MonitorManager.registerMonitors();
         registerInstantiations();
         registerFiles();
         registerCommands();
